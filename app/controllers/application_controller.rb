db4947bae83a5248b250818f229f5ca8547bacf8
risa86 <freex.risa1226@gmail.com>
2020-07-06 16:39:52 +0900

    [View]Sign_up
    完了
    Sign_up：name,name_kana,birth_day
    Scss未調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,18 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
-
+  protect_from_forgery with: :exception
+  before_action :configre_permitted_parameters, if: :devise_controller?
+  
   private
+
+  def configre_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:famly_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name_kana])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name_kana])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:birth_day])
+  end
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
