580ce5287818a3760c574c55e94e007cccdbbeba
matsuto04 <toshi.drdr@gmail.com>
2020-07-07 20:00:54 +0900

    コメントアウト等削除

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,10 +3,6 @@ class Item < ApplicationRecord
   has_one :buyer, class_name: "User"
   has_many :item_images, dependent: :destroy
   belongs_to :category
-  # belongs_to_active_hash :item_condition 
-  # belongs_to_active_hash :postage_payer
-  # belongs_to_active_hash :preparation_day
-  # belongs_to_active_hash :postage_type
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture_code
 
