8501ce7ca08dcb5939f2718415747dcaa9b498f5
risa86 <freex.risa1226@gmail.com>
2020-07-07 11:21:12 +0900

    [View]Sing_up,Edit
    完了
    Sign_up：address,tel
    Scss調整（上記内容はコメントアウトしてます）
    Edit：Signupの内容を貼っています。Signinできないため、View未確認

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,10 +8,16 @@ class ApplicationController < ActionController::Base
   def configre_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:famly_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name_kana])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name_kana])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:birth_day])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:post_code])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:prefecture_code])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:city])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:house_number])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:building_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:phone_number])
   end
 
   def basic_auth
