46a007b8cce425530d599329d4c93ceaef80e26d
risa86 <freex.risa1226@gmail.com>
2020-07-09 14:17:52 +0900

    root変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' } #sending_destinationテーブルに保存するため
-  # root 'home#index'
-  root 'items#show'
+  root 'home#index'
   resources :items
   resources :users 
   resources :cards, only: [:create, :new, :show, :destroy] 
